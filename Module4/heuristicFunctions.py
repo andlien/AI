8296326d424c088f94cdd763a474c73a281a3117
heronemis <simen@outlook.com>
2015-10-24 14:44:11 +0200

    Programmet kommer til 2048 ca 50 prosen av gangene med dybde 5 (definert som 2 i programmet vårt). Når den greier det, bruker den ca 30 sekunder

--- Module4/heuristicFunctions.py
+++ Module4/heuristicFunctions.py
@@ -1,16 +1,117 @@
+cords = []
+
 def getHeuristicValueForBoard(board):
-    sum = getEmptyCellsInBoard(board)
-    if isCellInCorner(getLargestCellInBoard(board)):
-        sum += 100
+    sum = getEmptyCellsInBoard(board) * 20
+    sortedBoard = list(board)
+    sortedBoard.sort()
+    #print("sortedBoard ", sortedBoard)
+    largetCell = getLargestCellInBoard(board)
+    #print(largetCell)
+
+    if isCellInCorner(largetCell):
+        sum += 100*getLargestCellInBoard(board)
+        # close = getCellsCloseToCorners(largetCell)
+        # if board[close[0]] == sortedBoard[1] or board[close[0]] == sortedBoard[2]:
+        #     sum += sortedBoard[1] * 50
+        # if board[close[1]] == sortedBoard[1] or board[close[1]] == sortedBoard[2]:
+        #     sum += sortedBoard[1] * 50
+
+        sortedIndex = getIndeciesForList(board)
+        #print(sortedIndex)
+        for i in xrange(1,len(sortedIndex)):
+            if board[sortedIndex[i]] < 4:
+                break
+            c1 = sortedIndex[i-1]
+            c1 = largetCell
+            c2 = sortedIndex[i]
+
+            #print("Dist: ", getDistance(c2,c1))
+
+            sum += (1 / (1 + getDistance(c2,c1))**2) * board[sortedIndex[i]]*20
+            #print((1 / 1 + getDistance(c2,c1)**2) * 2000)
 
     for cell in board:
-        sum += cell
+        if cell > 2:
+            sum += cell**2
+
 
     return sum
+
+def getHeuristicValueForBoard2(board):
+    sum = getEmptyCellsInBoard(board)*100
+    if isCellInCorner(getLargestCellInBoard(board)):
+        sum += 100*getLargestCellInBoard(board)
+
+    for cell in board:
+        if cell > 2:
+            sum += cell
+
+    return sum
+
+def getIndeciesForList(board):
+    sortedBoard = list(board)
+    sortedBoard.sort()
+    indices = []
+    for sortedCell in sortedBoard:
+        for cell in board:
+            if sortedCell == cell and cell not in indices:
+                indices.append(cell)
+
+
+    return indices
+
+def getDistance(cell1, cell2):
+    #cords = createCoordList()
+    global cords
+    c1 = cords[cell1]
+    c2 = cords[cell2]
+    return abs(c2[0]-c1[0]) + abs(c2[1]-c1[1])
 
 def isCellInCorner(cell):
     if cell in [0,3,12,15]:
         return True
+    return False
+
+            # 0, 1, 2, 3,
+            # 4, 5, 6, 7,
+            # 8, 9, 10, 11,
+            # 12, 13, 14, 15
+
+def createCoordList():
+    global cords
+    cords = []
+    for y in xrange(0,4):
+        for x in xrange(0,4):
+            cords.append([y,x])
+
+    #print(list)
+    #return list
+
+def getCellsCloseToCorners(cornerCell):
+    if cornerCell == 15:
+        return [14,11]
+    if cornerCell == 12:
+        return [8,13]
+    if cornerCell == 0:
+        return [1,4]
+    if cornerCell == 3:
+        return [2,7]
+    return [0]
+
+def isCellCloseToCornerCell(cell,cornerCell):
+    if cornerCell == 15:
+        if cell in [14,11]:
+            return True
+    if cornerCell == 12:
+        if cell in [8,13]:
+            return True
+    if cornerCell == 0:
+        if cell in [1,4]:
+            return True
+    if cornerCell == 3:
+        if cell in [2,7]:
+            return True
+
     return False
 
 def getEmptyCellsInBoard(board):
--- Module4/main2048.py
+++ Module4/main2048.py
@@ -1,16 +1,78 @@
 from visuals import GameWindow
 from Tkinter import *
 from random import randint
+from heuristicFunctions import *
+from multiprocessing import Process, Queue
 import time
 
 teller = 0
 
 root = Tk()
+
+
+
+maxSearchDepth = 3
 #root.geometry("250x150+300+300")
 #app = GameWindow()
+upScore = 0
+downScore = 0
+leftScore = 0
+rightScore = 0
 
+createCoordList()
+heighestScore = 0
+start = time.time()
 
 #main.mainloop()
+
+
+def runUp(q):
+    #global upScore
+
+    newBoard = slideUp(board,True)
+    if newBoard == None:
+        q.put(-1)
+        return
+    upScore = createTree(newBoard,0,1,True)
+    q.put(upScore)
+    #print("Up dune: ", upScore)
+    return
+
+def runDown(q):
+    #global downScore
+    newBoard = slideDown(board,True)
+    if newBoard == None:
+        q.put(-1)
+        return
+
+    downScore = createTree(newBoard,0,1,True)
+    q.put(downScore)
+    #print("downScore dune ", downScore)
+    return
+
+def runRight(q):
+    #global rightScore
+    newBoard = slideToTheRight(board,True)
+    if newBoard == None:
+        q.put(-1)
+        return
+
+    rightScore = createTree(newBoard,0,1,True)
+    q.put(rightScore)
+    #print("rightScore dune", rightScore)
+    return
+
+def runLeft(q):
+    #global leftScore
+    newBoard = slideToTheLeft(board,True)
+    if newBoard == None:
+        q.put(-1)
+        return
+
+    leftScore = createTree(newBoard,0,1,True)
+    q.put(leftScore)
+    #print("leftScore dune ", leftScore)
+    return
 
 
 #frame = Frame(width=768, height=576, bg="", colormap="new")
@@ -31,11 +93,12 @@ board = [   # A list of values currently present in the board on the form 2^x.
             0, 0, 0, 0,
             0, 0, 0, 0,
             0, 0, 0, 0,
-            0, 0, 0, 0
+            0, 1, 2, 2
         ]
-def slideToTheRight(board):
+def slideToTheRight(board,init):
     newBoard = list(board)
-    print("Board: " , newBoard)
+    #print("Board: " , newBoard)
+    didSomethingChange = False
 
     def canSlideRight(cellIndex):
         if cellIndex in [3,7,11,15]:
@@ -47,7 +110,7 @@ def slideToTheRight(board):
         else:
             return False
 
-    for cellIndex in reversed(xrange(0,len(board)-2)):
+    for cellIndex in reversed(xrange(0,len(board)-1)):
 
         if newBoard[cellIndex] == 0:
             continue
@@ -57,9 +120,11 @@ def slideToTheRight(board):
             if newBoard[currentIndex] == newBoard[currentIndex+1]:
                 newBoard[currentIndex+1] += 1
                 newBoard[currentIndex] = 0
-                print("Stacked!")
+                didSomethingChange = True
+                #print("Stacked!")
                 break #According to ios version, a cell can only stack ones per slide
             else:
+                didSomethingChange = True
                 newBoard[currentIndex+1] = newBoard[currentIndex]
                 newBoard[currentIndex] = 0
             currentIndex = currentIndex+1
@@ -67,11 +132,14 @@ def slideToTheRight(board):
                 break
             #print("Hei")
 
+    if not didSomethingChange and init:# and getEmptyCellsInBoard(newBoard) == 0:
+        #print("Dead end!!!")
+        return None
     return newBoard
 
-def slideToTheLeft(board):
+def slideToTheLeft(board,init):
     newBoard = list(board)
-
+    didSomethingChange = False
     def canSlideLeft(cellIndex):
         if cellIndex in [0,4,8,12]:
             return False
@@ -90,19 +158,24 @@ def slideToTheLeft(board):
             if newBoard[currentIndex] == newBoard[currentIndex-1]:
                 newBoard[currentIndex-1] += 1
                 newBoard[currentIndex] = 0
-                print("Stacked!")
+                didSomethingChange = True
+                #print("Stacked!")
                 break #According to ios version, a cell can only stack ones per slide
             else:
                 newBoard[currentIndex-1] = newBoard[currentIndex]
+                didSomethingChange = True
                 newBoard[currentIndex] = 0
             currentIndex = currentIndex-1
-            print("Hei")
+            #print("Hei")
 
+    if not didSomethingChange and init:# and :
+        #print("Dead end!!!")
+        return None
     return newBoard
 
-def slideUp(board):
+def slideUp(board,init):
     newBoard = list(board)
-
+    didSomethingChange = False
     def canSlideUp(cellIndex):
         if cellIndex in [0,1,2,3]:
             return False
@@ -123,21 +196,28 @@ def slideUp(board):
             if newBoard[currentIndex] == newBoard[currentIndex-4]:
                 newBoard[currentIndex-4] += 1
                 newBoard[currentIndex] = 0
-                print("Stacked!")
+                didSomethingChange = True
+                #print("Stacked!")
                 break #According to ios version, a cell can only stack ones per slide
             else:
                 newBoard[currentIndex-4] = newBoard[currentIndex]
                 newBoard[currentIndex] = 0
+                didSomethingChange = True
             currentIndex = currentIndex-4
             if currentIndex < 4:
                 break
-            print("Hei")
+            #print("Hei")
 
+    if not didSomethingChange and init:# and getEmptyCellsInBoard(newBoard) == 0:
+        #print("Dead end!!!")
+        return None
     return newBoard
 
 
-def slideDown(board):
+def slideDown(board,init):
+    #print("Baard: " , board)
     newBoard = list(board)
+    didSomethingChange = False
 
     def canSlideDown(cellIndex):
         if cellIndex in [12,13,14,15]:
@@ -159,16 +239,21 @@ def slideDown(board):
             if newBoard[currentIndex] == newBoard[currentIndex+4]:
                 newBoard[currentIndex+4] += 1
                 newBoard[currentIndex] = 0
-                print("Stacked!")
+                didSomethingChange = True
+                #print("Stacked!")
                 break #According to ios version, a cell can only stack ones per slide
             else:
                 newBoard[currentIndex+4] = newBoard[currentIndex]
                 newBoard[currentIndex] = 0
+                didSomethingChange = True
             currentIndex = currentIndex+4
             if currentIndex >= 12:
                 break
             #print("Hei")
 
+    if not didSomethingChange and init:# and getEmptyCellsInBoard(newBoard) == 0:
+        #print("Dead end!!!")
+        return None
     return newBoard
 
 def addNewCellToBoard(board):
@@ -178,7 +263,7 @@ def addNewCellToBoard(board):
         return None
 
     prob = randint(0,100)
-    print("Prob: ",prob)
+    #print("Prob: ",prob)
     newBoard = list(board)
 
     if prob > 90:
@@ -187,6 +272,61 @@ def addNewCellToBoard(board):
        newBoard[cell] = 1
 
     return newBoard
+
+
+
+
+def createTree(inputBoard, depht, prob, init):
+    global maxSearchDepth
+    if getEmptyCellsInBoard(board) > 4:
+        maxSearchDepth = 2
+    elif getEmptyCellsInBoard(board) == 2:
+        maxSearchDepth = 2
+    elif getEmptyCellsInBoard(board) <= 1:
+        maxSearchDepth = 3
+    else:
+        maxSearchDepth = 2
+    sum = 0
+    if maxSearchDepth <= depht:
+        return getHeuristicValueForBoard(inputBoard) * prob
+    for direction in xrange(0,4):
+        if init:
+            shiftedBoard = inputBoard
+        elif direction == 0:
+            shiftedBoard = slideDown(inputBoard,False)
+        elif direction == 1:
+            shiftedBoard = slideUp(inputBoard,False)
+        elif direction == 2:
+            shiftedBoard = slideToTheRight(inputBoard,False)
+        else:
+            shiftedBoard = slideToTheLeft(inputBoard,False)
+
+        if shiftedBoard is None:
+            break
+
+        #print("shiftedBoard: ", shiftedBoard)
+        childBoards = createAllEmptyBoardsCombos(shiftedBoard,1)
+        #print("childBoards:",childBoards)
+        for child in childBoards:
+            sum += createTree(child,depht +1, prob*0.9, False)
+
+        if  depht == 0 and  getEmptyCellsInBoard(board) <= 2:
+            childBoards = createAllEmptyBoardsCombos(shiftedBoard,2)
+            for child in childBoards:
+                sum += createTree(child,depht +1, prob*0.1,False)
+
+    return sum
+
+def createAllEmptyBoardsCombos(board,value):
+    openCells = []
+    for cell in xrange(0,len(board)):
+        if board[cell] == 0:
+            #for value in [1,2]:
+            newBoard = list(board)
+            newBoard[cell] = value
+            openCells.append(newBoard)
+
+    return openCells
 
 def getRandomOpenCellInBoard(board):
     openCells = []
@@ -211,9 +351,97 @@ def hei():
 
 
 board = addNewCellToBoard(board)
-board = slideDown(board)
+#board = slideDown(board)
 window.update_view( board ) # 1D list representing the board
 #window.after(1000,hei)
+
+def getProbs():
+    global board
+
+    global upScore
+    # global downScore,leftScore,rightScore
+    # downScore = createTree(slideDown(board),0,1)
+    # upScore = createTree(slideUp(board),0,1)
+    # rightScore = createTree(slideToTheRight(board),0,1)
+    # leftScore = createTree(slideToTheLeft(board),0,1)
+    #print("Down: ", down)
+    #print("Up: ", up)
+    #print("Right: ", right)
+    #print("Left: ", left)
+
+
+
+    up = Queue()
+    down = Queue()
+    left = Queue()
+    right = Queue()
+
+    t1 = Process(target=runUp, args=(up,))
+    #t1.daemon = True
+    t1.start()
+    #print("runUp started")
+    t2 = Process(target=runDown, args= (down,))
+    #t2.daemon = True
+    t2.start()
+    #print("runDown started")
+
+    t3 = Process(target=runLeft, args= (left,))
+    #t3.daemon = True
+    t3.start()
+    #print("runLeft started")
+    t4 = Process(target=runRight, args= (right,))
+    #t4.daemon = True
+    t4.start()
+    #print("runRight started")
+
+    t1.join()  # This waits until the thread has completed
+    t2.join()
+    t3.join()  # This waits until the thread has completed
+    t4.join()
+
+    #print("All threads finished?")
+
+    upScore = up.get()
+    downScore=down.get()
+    rightScore=right.get()
+    leftScore=left.get()
+
+
+    bestScore = downScore
+    bestNumber = 0
+    teller = 1
+    #print("Scores: ", [downScore,upScore,rightScore,leftScore])
+    for dir in [upScore,rightScore,leftScore]:
+        if dir > bestScore:
+            bestScore = dir
+            bestNumber = teller
+        teller += 1
+
+    if bestNumber == 0:
+        newBoard = slideDown(board,True)
+    if bestNumber == 1:
+        newBoard = slideUp(board,True)
+    if bestNumber == 2:
+        newBoard = slideToTheRight(board,True)
+    if bestNumber == 3:
+        newBoard = slideToTheLeft(board,True)
+
+    board = newBoard
+    largestTile = newBoard[getLargestCellInBoard(board)]
+    global heighestScore
+    global start
+    if largestTile > heighestScore:
+        heighestScore = largestTile
+        if 2 ** largestTile >= 512:
+            print("New highest tile: " + str(2 ** largestTile) + " - " + str((time.time() - start)/60) + " min")
+        else:
+            print("New highest tile: " + str(2 ** largestTile) + " - " + str(time.time() - start) + " sec")
+
+    board = addNewCellToBoard(board)
+    window.update_view( board )
+    window.after(1,getProbs)
+
+
 
 
 def leftKey(event):
@@ -253,8 +481,15 @@ root.bind('<Left>', leftKey)
 root.bind('<Right>', rightKey)
 root.bind('<Up>', upKey)
 root.bind('<Down>', downKey)
+root.bind('<space>', getProbs)
+
 root.focus_set()
 #root.pack()
+
+
+
+#createCoordList()
+window.after(1,getProbs)
 
 root.mainloop()
 
