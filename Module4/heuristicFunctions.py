1d771e27b91750a4799879c0fc7bc58c4029dae0
Anders Lien <alien_93@hotmail.no>
2015-10-26 21:58:17 +0100

    Skip duplicates in snake

--- Module4/heuristicFunctions.py
+++ Module4/heuristicFunctions.py
@@ -1,75 +1,9 @@
 from Module4.main2048 import slideDown, slideToTheLeft, slideToTheRight, slideUp
-from math import log
 
 cords = []
 for y in range(0,4):
     for x in range(0,4):
         cords.append([y,x])
-
-
-def getHeuristicValueForBoard3243(board):
-    sum = 0
-    sum += (getEmptyCellsInBoard(board) + 1) *10
-    optimalBoard = [3,2,1,0,4,5,6,7,11,10,9,8,12,13,14,15]
-    optimalBoard.reverse()
-    sortedIndex = getIndeciesForList(board)
-    sortedIndex.reverse()
-    for cell in range(0, len(board)):
-        if board[cell] <= 0:
-            continue
-
-        c1 = optimalBoard[cell]
-        c2 = sortedIndex[cell]
-
-        sum += ((1 / (1 + getDistance(c2,c1)))) #* (32 - cell*2))
-
-    return sum
-
-def getHeuristicValueForBoard900(board):
-    sum = 0
-    sum += (getEmptyCellsInBoard(board) + 1)
-    #optimalBoard = [3,2,1,0,4,5,6,7,11,10,9,8,12,13,14,15]
-    #optimalBoard.reverse()
-
-
-    sortedIndex = getIndeciesForList(board)
-    sortedIndex.reverse()
-    largetCell = getLargestCellInBoard(board)
-
-    if isCellInCorner(largetCell):
-        sum += 20
-    #print(sortedIndex)
-    for i in range(1,int(len(sortedIndex))):
-        if board[sortedIndex[i]] <= 2:
-              break
-        c1 = sortedIndex[i-1]
-        #c1 = largetCell
-        c2 = sortedIndex[i]
-        #sum += board[c1]
-
-        #print("Dist: ", getDistance(c2,c1))
-
-        #sum += ((1 / (1 + getDistance(c2,c1))**2) * (16-i) ) * 0.5
-        dist = getDistance(c2,c1)
-        if dist == 1 and not board[c1] == board[c2]:
-            #print("skjer ofte")
-            sum += 1
-        elif dist == 1:
-            sum += 0.5
-        elif dist == 2:
-            sum += 0.2
-        #
-        # if not board[c1] == board[c2]:
-        #     sum += ((1 / (1 + getDistance(c2,c1))**2) * (32-i))
-
-        #c1 = largetCell
-        #sum += ( (1 / (1 + getDistance(c2,c1))**2) * (16-i) ) * 0.5
-
-    if getEmptyCellsInBoard(board) <= 0:
-        teller = isBoardStuck(board)
-        sum += 1/teller
-
-    return sum
 
 
 def isBoardStuck(board):
@@ -90,180 +24,24 @@ def isBoardStuck(board):
     return teller
 
 
-def getHeuristicValueForBoard345(board):
-    sum = 0
-    #optimalBoard = [3,2,1,0,4,5,6,7,11,10,9,8,12,13,14,15]
-    #optimalBoard.reverse()
-    sortedIndex = getIndeciesForList(board)
-    if sortedIndex[0] == 15:
-        sum += 1
-        if sortedIndex[1] == 14:
-            sum += 2
-            if sortedIndex[2] == 13:
-                sum += 3
-            if sortedIndex[3] == 12:
-                sum += 4
-
-
-    sum += (getEmptyCellsInBoard(board))# ** 2
-
-    if sum <= 1:
-        teller = isBoardStuck(board)
-        sum += 1/teller
-
-    # largetCell = getLargestCellInBoard(board)
-    # if isCellInCorner(largetCell):
-    #     sum += 2
-    return sum
-
-def getHeuristicValueForBoard4322(board):
-    sum = getEmptyCellsInBoard(board)
-    sortedBoard = list(board)
-    sortedBoard.sort()
-    #print("sortedBoard ", sortedBoard)
-    largetCell = getLargestCellInBoard(board)
-    #print(largetCell)
-
-
-    sortedIndex = getIndeciesForList(board)
-    #print(sortedIndex)
-    for i in range(1,len(sortedIndex)):
-        if board[sortedIndex[i]] < 4:
-            break
-        c1 = sortedIndex[i-1]
-        c1 = largetCell
-        c2 = sortedIndex[i]
-
-        #print("Dist: ", getDistance(c2,c1))
-
-        sum += (1 / (1 + getDistance(c2,c1))**2) * board[sortedIndex[i]]*20
-
-
-    for x in range(0,len(board)-1):
-        if sortedBoard[x] <= 3:
-            break
-        if sortedBoard[x] == sortedBoard[x-1]:
-            sum -= 0#sortedBoard[x] ** 2
-        else:
-            sum += sortedBoard[x] ** 2
-
-
-    if isCellInCorner(largetCell):
-        sum += board[largetCell] ** 2
-    else:
-        sum -= 0#board[largetCell] ** 2
-
-    return sum
-
-def getHeuristicValueForBoard2222(board):
-    sum = getEmptyCellsInBoard(board) * 20
-    sortedBoard = list(board)
-    sortedBoard.sort()
-    #print("sortedBoard ", sortedBoard)
-    largetCell = getLargestCellInBoard(board)
-    #print(largetCell)
-
-    if isCellInCorner(largetCell):
-        sum += 100*getLargestCellInBoard(board)
-        # close = getCellsCloseToCorners(largetCell)
-        # if board[close[0]] == sortedBoard[1] or board[close[0]] == sortedBoard[2]:
-        #     sum += sortedBoard[1] * 50
-        # if board[close[1]] == sortedBoard[1] or board[close[1]] == sortedBoard[2]:
-        #     sum += sortedBoard[1] * 50
-
-        sortedIndex = getIndeciesForList(board)
-        #print(sortedIndex)
-        for i in range(1,len(sortedIndex)):
-            if board[sortedIndex[i]] < 4:
-                break
-            c1 = sortedIndex[i-1]
-            c1 = largetCell
-            c2 = sortedIndex[i]
-
-            #print("Dist: ", getDistance(c2,c1))
-
-            sum += (1 / (1 + getDistance(c2,c1))**2) * board[sortedIndex[i]]*20
-            #print((1 / 1 + getDistance(c2,c1)**2) * 2000)
-
-    for cell in board:
-        if cell > 2:
-            sum += cell**2
-
-
-    return sum
-
-
-def getHeuristicValueForBoard34234(board):
-    sum = 0#getEmptyCellsInBoard(board)
-    sortedIndex = getIndeciesForList(board)
-    #print(sortedIndex)
-    if sortedIndex[0] == 15:
-        print("Ja, det skjer");
-        sum += 1
-        if sortedIndex[1] == 14:
-            sum += 1
-
-    # for i in range(1,len(sortedIndex)):
-    #     if board[sortedIndex[i]] < 4:
-    #         break
-    #     c1 = sortedIndex[i-1]
-    #     c1 = largetCell
-    #     c2 = sortedIndex[i]
-    #
-    #     #print("Dist: ", getDistance(c2,c1))
-    #
-    #     sum += (1 / (1 + getDistance(c2,c1))**2) * board[sortedIndex[i]]*20
-
-    return sum
-
-
-def getHeuristicValueForBoard666(board):
-    sum = getEmptyCellsInBoard(board)
-    if isCellInCorner(getLargestCellInBoard(board)):
-        sum += 2
-
-    # for cell in board:
-    #     if cell > 2:
-    #         sum += cell
-
-    return sum
-
-
-
 def getHeuristicValueForBoard(board):
-    sum = 0#getEmptyCellsInBoard(board)*20
-    #sum = getEmptyCellsInBoard(board)*20
-    #sum += 2 ** board[getLargestCellInBoard(board)]
+    sum = 0
 
     largestCell = getLargestCellInBoard(board)
     if isCellInCorner(largestCell):
         sum += board[largestCell] ** 2
 
         # snake
-        neighbours = getNeighbourCells(largestCell)
-        if board[neighbours[0]] > board[neighbours[1]]:
-            diff = largestCell - neighbours[0]
-        else:
-            diff = largestCell - neighbours[1]
-        #first snakerow
-        for i in range(1, 4):
-            if board[largestCell-(i-1)*diff] < board[largestCell-i*diff]:
-                break
-            sum += ((board[largestCell-i*diff] * 1.5 * i) ** 2)
-        # #second snakerow
-        # if
-        # for i in range(1, 4):
-
-
+        sum += getSnakeScore(board, largestCell)
 
     for cell in range(0, len(board)):
         sum += compareToCellsAround(cell, board)
 
-    #sum += scanColumns(board)
-    #sum += scanRows(board)
-
     sortedIndex = getIndeciesForList(board)
     sortedIndex.reverse()
+
+
+
     for cell in range(0, 15):
         if board[sortedIndex[cell]] <= 2:
              break
@@ -278,7 +56,7 @@ def getHeuristicValueForBoard(board):
 
         #print("Dist: ", getDistance(c2,c1))
 
-        sum += (1 / (1 + getDistance(c2,c1))**2) * board[sortedIndex[cell]]
+        # sum += (1 / (1 + getDistance(c2,c1))**2) * board[sortedIndex[cell]]
             #if getDistance(sortedIndex[cell],sortedIndex[cell+1]) > 1:
                 #sum -= board[sortedIndex[cell]]
             #sum += board[sortedIndex[cell]]
@@ -298,7 +76,6 @@ def getHeuristicValueForBoard(board):
 #def areTheFourLargestTilesOnTheSameRow(board):
 
 
-
 def getIndeciesForList(board):
     sortedBoard = list(board)
     sortedBoard.sort()
@@ -311,7 +88,36 @@ def getIndeciesForList(board):
 
     return indices
 
+def getSnakeScore(board, largestCell):
+    sum = 0
+    neighbours = getNeighbourCells(largestCell)
+    if board[neighbours[0]] > board[neighbours[1]]:
+        diff = largestCell - neighbours[0]
+        lastSnakeCell = neighbours[1]
+    else:
+        diff = largestCell - neighbours[1]
+        lastSnakeCell = neighbours[0]
+    #first snakerow
+    for i in range(1, 4):
+        if board[largestCell-(i-1)*diff] < board[largestCell-i*diff]:
+            break
+        if board[largestCell-(i-1)*diff] == board[largestCell-i*diff]:
+            continue
+        sum += ((board[largestCell-i*diff] * 3) ** 2)
+    #second snakerow
+    else:
+        nextRowStart = lastSnakeCell - diff * 3
 
+        if board[nextRowStart] < board[largestCell-3*diff]:
+            sum += ((board[nextRowStart] * 3) ** 2)
+            diff = -diff
+            for i in range(1, 4):
+                if board[nextRowStart-(i-1)*diff] < board[nextRowStart-i*diff]:
+                    break
+                if board[nextRowStart-(i-1)*diff] == board[nextRowStart-i*diff]:
+                    continue
+                sum += ((board[nextRowStart-i*diff] * 3) ** 2)
+    return sum
 
 
 
